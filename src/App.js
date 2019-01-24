67c76b6c8c9ddd83a1d379904038210a3aea91fd
AshwiniMohan05 <ashwini@quintype.com>
2019-01-24 12:24:34 +0530

    I have added a state in this code where if the work done is true, it goes above the line else if it is false it goes below the line

--- src/App.js
+++ src/App.js
@@ -3,16 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 classname = "App-title"> Welcome to React </h1>
+      <p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      
+      <TodoList1/>
+      </header>
       </div>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  
+  
+  class TodoList1 extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        input: '',  
+        todo: [ { todo : "Buy Milk" ,done : "false"}, {todo : "Keep Alarm" , done : "true"} , {todo : "Watch FRIENDS" , done : "true"}, {todo : "ABC" , done : "false"}, {todo :"Dinner Time" , done :"true"} ]
+      }
+    }
+    
+    render() {
+      return (
+        <div>
+        <input type="text" value ={this.state.input} />
+        <input type = "submit" onClick />
+        <div>
+        { this.state.todo
+          .filter (item => item.done == "true")
+          .map((iterator) => <div>{iterator.todo}, {iterator.done}</div> )} 
+          <hr/>
+          
+          {this.state.todo
+            .filter( item => item.done == "false")
+            .map((iterator) => <div> {iterator.todo}, {iterator.done} </div>)}
+            
+            </div>
+            </div>
+            
+            )
+          }
+        }
+        
+        export default App;
+        
